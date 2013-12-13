31bf482b087c2b8f3d22e54a3b6dc29436c61bd4
Justin Smith <justins@criticalmass.com>
2013-12-13 12:20:48 -0600

    Added comments to holiday star, updated HTTPClient

--- holiday_star_fade/holiday_star_fade.ino
+++ holiday_star_fade/holiday_star_fade.ino
@@ -1,14 +1,24 @@
+/* HOLIDAY STAR SKETCH 
+   Dec 2013 - Justin Smith
+   Developed for Hack Yourself a Merry Little Xmas - Critical Mass, Chicago
+*/
+
+// Declare your variables that won't change
 int redLed = 3;
 int greenLed = 5;
 int blueLed = 6;
 
+// Run the setup loop
 void setup()  { 
+  // Tell the code what the pins that you described need to do
   pinMode(redLed, OUTPUT);   
   pinMode(greenLed, OUTPUT);   
   pinMode(blueLed, OUTPUT);   
 } 
 
+// Run the code
 void loop()  {
+  // Call a function that we declare below
   fadeIn(redLed, 15);
   fadeOut(redLed, 5);
   fadeIn(greenLed, 15);
@@ -17,9 +27,11 @@ void loop()  {
   fadeOutAll(5);
 }
 
-int fadeIn(int color, int duration){
+// Declare functions to fade LEDs in and out
+
+int fadeIn(int color, int duration){ // declare the things that you want to change inside the parentheses 
   for(int fade=0; fade<=255; fade+=1){
-        analogWrite(color, fade); 
+        analogWrite(color, fade); // reuse the variables that you declared in the function call 
         delay(duration);
    }
 }
