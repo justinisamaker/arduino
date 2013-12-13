a585bffb68f70d69ece7f53ff57a63317ddf14bf
Justin Smith <justins@criticalmass.com>
2013-12-13 12:28:10 -0600

    Updated comments for Holiday Star

--- holiday_star_fade/holiday_star_fade.ino
+++ holiday_star_fade/holiday_star_fade.ino
@@ -1,25 +1,31 @@
 /* HOLIDAY STAR SKETCH 
    Dec 2013 - Justin Smith
    Developed for Hack Yourself a Merry Little Xmas - Critical Mass, Chicago
+   
+   PARTS:
+     Tri-Color Breakout Board - https://www.sparkfun.com/products/11588
+     Arduino (any flavor)
+   
+   Hookup:
+     Tri-color to 5v, Ground, and pins 3, 5, 6
 */
 
 // Declare your variables that won't change
-int redLed = 3;
-int greenLed = 5;
-int blueLed = 6;
+int redLed = 3; // Declare the pins that you're plugging the LED leads into
+int greenLed = 5; // We use pins 3, 5, and 6 because they are Pulse Width Modulation (PWM) pins
+int blueLed = 6; // PWM pins are denoted by a "~" on the board, and can send values between 0 and 255
 
 // Run the setup loop
 void setup()  { 
-  // Tell the code what the pins that you described need to do
-  pinMode(redLed, OUTPUT);   
+  pinMode(redLed, OUTPUT); // Tell the code what the pins that you described need to do (input or output) 
   pinMode(greenLed, OUTPUT);   
   pinMode(blueLed, OUTPUT);   
 } 
 
 // Run the code
 void loop()  {
-  // Call a function that we declare below
-  fadeIn(redLed, 15);
+  // Call a custom function that we declare below to keep code concise
+  fadeIn(redLed, 15); // This function call is saying "fade in the red led for 15 milliseconds
   fadeOut(redLed, 5);
   fadeIn(greenLed, 15);
   fadeOut(greenLed, 5);
@@ -30,9 +36,9 @@ void loop()  {
 // Declare functions to fade LEDs in and out
 
 int fadeIn(int color, int duration){ // declare the things that you want to change inside the parentheses 
-  for(int fade=0; fade<=255; fade+=1){
-        analogWrite(color, fade); // reuse the variables that you declared in the function call 
-        delay(duration);
+  for(int fade=0; fade<=255; fade+=1){ // A for loop sets a variable and adds 1 to the number until it reaches the top limit
+        analogWrite(color, fade); // write the value of fade to the specified pin 
+        delay(duration); // wait a few milliseconds before incrementing the for loop
    }
 }
 
