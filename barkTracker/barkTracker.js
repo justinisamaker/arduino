05923d87b8c47ad5e5de073c9b5ad011fafb9f14
justintsmith <justin@justintsmith.com>
2015-01-19 22:17:55 -0600

    Updated barkTracker.js with moment support to clean up timestamp reporting

--- barkTracker/barkTracker.js
+++ barkTracker/barkTracker.js
@@ -1,14 +1,16 @@
-/* -----------------------------
+/* -----------------------------                                                
   BARK TRACKER
   Justin Smith - justin@isamaker.com
   @justinIsAMaker
-
-  Listen for sound over a certain threshold, report that sound to a feed on data.sparkfun.com
+ 
+  Listen for sound over a certain threshold, report that sound to a feed on data
 ------------------------------ */
 
-// Require MRAA and Request libraries
+// Require libraries
 var m = require('mraa');
 var request = require('request');
+var moment = require('moment');
+var momentTimezone = require('moment-timezone');
 
 // Do a sanity check to make sure that MRAA is loaded
 console.log('MRAA Version: ' + m.getVersion());
@@ -17,7 +19,7 @@ console.log('MRAA Version: ' + m.getVersion());
 var soundSensor = new m.Aio(0);
 
 // Set the sound threshold
-var threshold = 900;
+var threshold = 800;  
 
 // Run the function to start out
 checkSoundLevels();
@@ -32,23 +34,26 @@ function checkSoundLevels(){
 
   // If the sound is higher than the threshold, make the request
   if(soundValue >= threshold){
+    // Set the current time with my timezone, format the date and time
+    var currentTime = moment().tz("America/Chicago").format('HH:mm:ss - MM/DD/YYYY');
     // Use the request library to hit the Sparkfun URL - make sure you replace the applicable parts with your data
-		request('http://data.sparkfun.com/input/[INSERT PUBLIC KEY]?private_key=[INSERT PRIVATE KEY]&soundlevel=' + soundValue, function(error, response, body){
+    request('http://data.sparkfun.com/input/[INSERT PUBLIC KEY]?private_key=[INSERT PRIVATE KEY]&soundlevel=' + soundValue + '&localtime=' + currentTime, function(error, response, body){
       console.log(response.statusCode);
-      // If the response is good, wait 5 seconds before we start checking again
+
+      // If the response is good, wait 10 seconds before we start checking again 
       if(response.statusCode === 200){
-      	console.log('posted successfully with a sound value of ' + soundValue);
-      	setTimeout(function(){
+        console.log('posted successfully with a sound value of ' + soundValue + ' at ' + currentTime); 
+        setTimeout(function(){
           setTimeout(checkSoundLevels, 100);
-        }, 5000);
+        }, 10000);
       } else {
-      	console.log('oops, there was an error');
+        console.log('oops, there was an error');
         console.log(response.statusCode + ' :::: ' + response.body);
-      	setTimeout(checkSoundLevels, 100);
+        setTimeout(checkSoundLevels, 100);
       }
-		});
+    });
   } else {
-  	setTimeout(checkSoundLevels, 100);
-    console.log(soundValue);
+    setTimeout(checkSoundLevels, 100);
+    // console.log(soundValue);
   }
 }
